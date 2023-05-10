4412eac2ad6f388381fba7d550a479023c82e25f
leesfact <lky110408@gmail.com>
2023-05-10 17:46:44 +0900

    init: OAuth2 JWT 회원가입 완료

---
+++ oauth2-back/src/main/java/com/study/oauth2/entity/User.java
@@ -0,0 +1,42 @@
+package com.study.oauth2.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.study.oauth2.security.PrincipalUser;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+	private int userId;
+	private String email;
+	private String password;
+	private String name;
+	private String provider;
+	
+	private List<Authority> authorities;
+	
+	
+	public PrincipalUser toPrincipal() {
+		
+		List<String> roles = new ArrayList<>();
+		
+		authorities.forEach(authority ->{
+			roles.add(authority.getRole().getRoleName());
+		});
+		
+		return PrincipalUser.builder()
+				.userId(userId)
+				.email(email)
+				.password(password)
+				.authorities(authorities)
+				.build();
+	}
+}
---
+++ oauth2-back/src/main/java/com/study/oauth2/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.study.oauth2.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.study.oauth2.entity.Authority;
+import com.study.oauth2.entity.User;
+
+
+@Mapper
+public interface UserRepository {
+	// 이메일 중복확인
+	public User findUserByEmail(String email);
+	
+	// 유저 등록
+	public int saveUser (User user);
+	public int saveAuthority(Authority authority);
+	
+
+	
+}
